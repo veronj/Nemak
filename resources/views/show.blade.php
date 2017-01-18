bc91a9ee6bb0a85751ca6873cb0af203619fd9db
Jb Snow <mesquinos@hotmail.com>
2017-01-18 20:36:23 +0100

    clean up view

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -2,151 +2,148 @@
 
 @section('content')
     <div class="container">
-
-            <div class="col-md-12 ">
-
-                    @if (Session::has('moved'))
-                        <div class="flash alert-info">
-                            <p>{{ Session::get('moved') }}</p>
-                        </div>
-                    @endif
-
-                {{--@if ($messages->any())--}}
-                {{--@foreach($messages as $message)--}}
-                {{--{{ $error }}--}}
-                {{--@endforeach--}}
-                {{--@endif--}}
-
-                {{ $commandant->name }} // Position x : {{ $commandant->x_position }} -
-                Position y : {{ $commandant->y_position }}
-
-                <div class="panel-body">
-                    <table class="table table-bordered table-stars">
-                        <tr>
-                            <td>
-                                <br>{{ isset($nearStars[0]->name) ? $nearStars[0]->name : 'Void' }} @if (isset($nearStars[0]->name))
-                                    <br> <a href="{{ url('/moving/North-West') }}">Rejoindre</a>  @endif</td>
-                            <td>
-                                <br>{{ isset($nearStars[1]->name) ? $nearStars[1]->name : 'Void' }} @if (isset($nearStars[1]->name))
-                                    <br><a href="{{ url('/moving/North') }}">Rejoindre</a> @endif</td>
-                            <td>
-                                <br>{{ isset($nearStars[2]->name) ? $nearStars[2]->name : 'Void' }} @if (isset($nearStars[2]->name))
-                                    <br>
-                                    <button><a href="{{ url('/moving/North-East') }}">Rejoindre</a></button> @endif</td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <br>{{ isset($nearStars[3]->name) ? $nearStars[3]->name : 'Void' }} @if (isset($nearStars[3]->name))
-                                    <br>
-                                    <button><a href="{{ url('/moving/West') }}">Rejoindre</a></button> @endif</td>
-                            <td><br><br>{{ isset($nearStars[4]->name) ? $nearStars[4]->name : 'Void' }} <br></td>
-                            <td>
-                                <br>{{ isset($nearStars[5]->name) ? $nearStars[5]->name : 'Void' }} @if (isset($nearStars[5]->name))
-                                    <br>
-                                    <button><a href="{{ url('/moving/East') }}">Rejoindre</a></button> @endif</td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <br>{{ isset($nearStars[6]->name) ? $nearStars[6]->name : 'Void' }} @if (isset($nearStars[6]->name))
-                                    <br>
-                                    <button><a href="{{ url('/moving/South-West') }}">Rejoindre</a></button> @endif</td>
-                            <td>
-                                <br>{{ isset($nearStars[7]->name) ? $nearStars[7]->name : 'Void' }} @if (isset($nearStars[7]->name))
-                                    <br><a href="{{ url('/moving/South') }}">Rejoindre</a> @endif</td>
-                            <td>
-                                <br>{{ isset($nearStars[8]->name) ? $nearStars[8]->name : 'Void' }} @if (isset($nearStars[8]->name))
-                                    <br>
-                                    <button><a href="{{ url('/moving/South-East') }}">Rejoindre</a></button> @endif</td>
-                        </tr>
-                    </table>
+        <div class="row">
+        <div class="col-md-12 ">
+            @if (Session::has('moved'))
+                <div class="flash alert-info">
+                    <p>{{ Session::get('moved') }}</p>
                 </div>
-                        <button type="button" class="btn btn-primary" data-toggle="collapse" data-target="#content">Show</button>
-                        <div class="col-md-4 ">
-                    <table class="table table-hover table-condensed bg-info">
-                        {!! Form::open(array('method' => 'post', 'route' => 'order.store')) !!}
+            @endif
 
-                        <th>Nom</th>
-                        <th>Hommes</th>
-                        <th>Lasers</th>
-                        <th>Type</th>
-                        <th>Population</th>
-                        <th>Status</th>
-                        <th>Commandant</th>
-                        <th>Type</th>
-                        <th>Hommes</th>
-                        <th> Lasers </th>
-                        {{ Form::text('commandant_id', $commandant->id, ['hidden' => 'hidden']) }}
-                        @foreach($nearStars as $star)
-                            <tr>
-                                <td>{{ $star->name }}</td>
-                                <td>{{ $star->men }}</td>
-                                <td>{{ $star->lasers }}</td>
-                                <td>{{ $star->type }}</td>
-                                <td>{{ $star->population }}</td>
-                                <td>{{ $star->status }}</td>
-                                <td>{{ $star->commandant_id }}</td>
+            {{ $commandant->name }} // Position x : {{ $commandant->x_position }} -
+            Position y : {{ $commandant->y_position }}
 
-                                @if ($star['attack'])
+            <div class="panel-body">
+                <table class="table table-bordered table-stars">
+                    <tr>
+                        <td>
+                            <br>{{ isset($nearStars[0]->name) ? $nearStars[0]->name : 'Void' }} @if (isset($nearStars[0]->name))
+                                <br> <a href="{{ url('/moving/North-West') }}">Rejoindre</a>  @endif</td>
+                        <td>
+                            <br>{{ isset($nearStars[1]->name) ? $nearStars[1]->name : 'Void' }} @if (isset($nearStars[1]->name))
+                                <br><a href="{{ url('/moving/North') }}">Rejoindre</a> @endif</td>
+                        <td>
+                            <br>{{ isset($nearStars[2]->name) ? $nearStars[2]->name : 'Void' }} @if (isset($nearStars[2]->name))
+                                <br>
+                                <button><a href="{{ url('/moving/North-East') }}">Rejoindre</a></button> @endif</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <br>{{ isset($nearStars[3]->name) ? $nearStars[3]->name : 'Void' }} @if (isset($nearStars[3]->name))
+                                <br>
+                                <button><a href="{{ url('/moving/West') }}">Rejoindre</a></button> @endif</td>
+                        <td><br><br>{{ isset($nearStars[4]->name) ? $nearStars[4]->name : 'Void' }} <br></td>
+                        <td>
+                            <br>{{ isset($nearStars[5]->name) ? $nearStars[5]->name : 'Void' }} @if (isset($nearStars[5]->name))
+                                <br>
+                                <button><a href="{{ url('/moving/East') }}">Rejoindre</a></button> @endif</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <br>{{ isset($nearStars[6]->name) ? $nearStars[6]->name : 'Void' }} @if (isset($nearStars[6]->name))
+                                <br>
+                                <button><a href="{{ url('/moving/South-West') }}">Rejoindre</a></button> @endif</td>
+                        <td>
+                            <br>{{ isset($nearStars[7]->name) ? $nearStars[7]->name : 'Void' }} @if (isset($nearStars[7]->name))
+                                <br><a href="{{ url('/moving/South') }}">Rejoindre</a> @endif</td>
+                        <td>
+                            <br>{{ isset($nearStars[8]->name) ? $nearStars[8]->name : 'Void' }} @if (isset($nearStars[8]->name))
+                                <br>
+                                <button><a href="{{ url('/moving/South-East') }}">Rejoindre</a></button> @endif</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <div class="row">
+        <div class="col-md-6 bg-info">
+            <table class="table table-hover table-condensed">
+                {!! Form::open(array('method' => 'post', 'route' => 'order.store')) !!}
 
-                                    <td>{{ Form::text('star[]', $star->id, ['hidden' => 'hidden']) }}{{ Form::select('type[]', ['A' => 'Attack', 'D' => 'Defense'], $star['attack']['type']) }}</td>
-                                    <td>{{ Form::text('men[]', $star['attack']['men'], ['class' => 'form-control']) }}</td>
-                                    <td>{{ Form::text('lasers[]', $star['attack']['lasers'], array('class' => 'form-control')) }}</td>
-                                @else
-                                    <td>{{ Form::text('star[]', $star->id, ['hidden' => 'hidden']) }}{{ Form::select('type[]', ['A' => 'Attack', 'D' => 'Defense']) }}</td>
-                                    <td>{{ Form::text('men[]', 0, ['class' => 'form-control']) }}</td>
-                                    <td>{{ Form::text('lasers[]', 0, array('class' => 'form-control')) }}</td>
-                                @endif
+                <th>Nom</th>
+                <th>Hommes</th>
+                <th>Lasers</th>
+                <th>Type</th>
+                <th>Population</th>
+                <th>Status</th>
+                <th>Commandant</th>
+                <th>Type</th>
+                <th>Hommes</th>
+                <th> Lasers </th>
+                {{ Form::text('commandant_id', $commandant->id, ['hidden' => 'hidden']) }}
+                @foreach($nearStars as $star)
+                    <tr>
+                        <td>{{ $star->name }}</td>
+                        <td>{{ $star->men }}</td>
+                        <td>{{ $star->lasers }}</td>
+                        <td>{{ $star->type }}</td>
+                        <td>{{ $star->population }}</td>
+                        <td>{{ $star->status }}</td>
+                        <td>{{ $star->commandant_id }}</td>
 
-                            </tr>
-                        @endforeach
-                        {!! Form::submit('Valider', array('class' => 'btn btn-success')) !!}
-                        {!! Form::close() !!}
-                    </table>
+                        @if ($star['attack'])
 
-                    <table class="table table-hover table-condensed bg-info">
-                                {!! Form::open(array('method' => 'post', 'route' => 'orderMission.store')) !!}
+                            <td>{{ Form::text('star[]', $star->id, ['hidden' => 'hidden']) }}{{ Form::select('type[]', ['A' => 'Attack', 'D' => 'Defense'], $star['attack']['type']) }}</td>
+                            <td>{{ Form::text('men[]', $star['attack']['men'], ['class' => 'form-control']) }}</td>
+                            <td>{{ Form::text('lasers[]', $star['attack']['lasers'], array('class' => 'form-control')) }}</td>
+                        @else
+                            <td>{{ Form::text('star[]', $star->id, ['hidden' => 'hidden']) }}{{ Form::select('type[]', ['A' => 'Attack', 'D' => 'Defense']) }}</td>
+                            <td>{{ Form::text('men[]', 0, ['class' => 'form-control']) }}</td>
+                            <td>{{ Form::text('lasers[]', 0, array('class' => 'form-control')) }}</td>
+                        @endif
 
-                                <th>Type</th>
-                                <th>Navettes</th>
+                    </tr>
+                @endforeach
+                {!! Form::submit('Valider', array('class' => 'btn btn-success')) !!}
+                {!! Form::close() !!}
+            </table>
+        </div>
+        <div class="col-md-1 bg-warning">
+        </div>
+        <div class="col-md-4 bg-info">
+            <table class="table table-hover table-condensed ">
+                {!! Form::open(array('method' => 'post', 'route' => 'orderMission.store')) !!}
+                <th>Type</th>
+                <th>Navettes</th>
+                {{ Form::text('commandant_id', $commandant->id, ['hidden' => 'hidden']) }}
+                @foreach($nearStars as $star)
+                    <tr>
+                        @if ($star['mission'])
 
-                                {{ Form::text('commandant_id', $commandant->id, ['hidden' => 'hidden']) }}
-                                @foreach($nearStars as $star)
-                                    <tr>
-                                        @if ($star['mission'])
+                            <td>{{ Form::text('star[]', $star->id, ['hidden' => 'hidden']) }}{{ Form::select('type[]', ['C' => 'Commerce', 'P' => 'Pillage'], $star['mission']['type']) }}</td>
+                            <td>{{ Form::text('ships[]', $star['mission']['ships'], ['class' => 'form-control']) }}</td>
 
-                                            <td>{{ Form::text('star[]', $star->id, ['hidden' => 'hidden']) }}{{ Form::select('type[]', ['C' => 'Commerce', 'P' => 'Pillage'], $star['mission']['type']) }}</td>
-                                            <td>{{ Form::text('ships[]', $star['mission']['ships'], ['class' => 'form-control']) }}</td>
+                        @else
+                            <td>{{ Form::text('star[]', $star->id, ['hidden' => 'hidden']) }}{{ Form::select('type[]', ['C' => 'Commerce', 'P' => 'Pillage']) }}</td>
+                            <td>{{ Form::text('ships[]', 0, ['class' => 'form-control']) }}</td>
 
-                                        @else
-                                            <td>{{ Form::text('star[]', $star->id, ['hidden' => 'hidden']) }}{{ Form::select('type[]', ['C' => 'Commerce', 'P' => 'Pillage']) }}</td>
-                                            <td>{{ Form::text('ships[]', 0, ['class' => 'form-control']) }}</td>
+                        @endif
 
-                                        @endif
-
-                                    </tr>
-                                @endforeach
-                                {!! Form::submit('Mission', array('class' => 'btn btn-success')) !!}
+                    </tr>
+                @endforeach
+                {!! Form::submit('Mission', array('class' => 'btn btn-success')) !!}
+                {!! Form::close() !!}
+            </table>
+        </div>
+        <div class="col-md-1 bg-success">
+            <table class="table table-hover table-condensed">
+                {{ Form::text('commandant_id', $commandant->id, ['hidden' => 'hidden']) }}
+                @foreach($nearStars as $star)
+                    <tr>
+                        @if ($star['mission'])
+                            <td>
+                                {!! Form::open(array('method' => 'DELETE', 'route' => ['orderMission.destroy', $star['mission']['id']])) !!}
+                                {!! Form::submit('Delete', ['class' => 'btn btn-danger btn-block']) !!}
                                 {!! Form::close() !!}
-                            </table>
-</div>
-                        <div class="col-md-2"></div>
-                        <div class="col-md-4">
+                            </td>
+                       @else
+                            <td></td>
+                       @endif
+                    </tr>
+                @endforeach
+            </table>
+        </div>
+    </div>
 
-                            <table class=" collapse table table-hover bg-info" id="content">
-                                <th>Type</th>
-                                <th>Hommes</th>
-                                <th>Lasers</th>
-
-                                @foreach($commandant->stars as $star)
-                                    <tr>
-                                        <td>{{ $star->name }}</td>
-                                        <td>{{ $star->men }}</td>
-                                        <td>{{ $star->lasers }}</td>
-
-                                    </tr>
-                                @endforeach
-                            </table>
-                        </div>
 
 
                 {{--<?php--}}
@@ -155,7 +152,5 @@
                 {{--?>--}}
                 {{--<a href="{{ url('/movingJson/' . $northJson) }}">North</a>--}}
                 {{--<a href="{{ url('/movingJson/' . $southJson) }}">South</a>--}}
-            </div>
 
-    </div>
 @endsection
