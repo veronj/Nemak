f63ff9a3836fa75336e282a0b02bcd234bfb8d05
Jb Snow <mesquinos@hotmail.com>
2017-01-19 20:16:21 +0100

    added mission and buy order

---
+++ app/Http/Controllers/OrderMissionController.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+use App\Order_mission;
+
+class OrderMissionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $input = $request->all();
+        $this->validate($request, array());
+        $count = count($input['star']);
+
+        for ($i = 0; $i < $count; $i++) {
+            $order = Order_mission::where('turn_id', 1)->where('commandant_id', $input['commandant_id'])->where('star_id', $input['star'][$i])->first();
+            if (!$order) {
+                if ($input['ships'][$i] > 0) {
+                    $order = new Order_mission;
+                    $order->turn_id = 1;
+                    $order->commandant_id = $input['commandant_id'];
+                    $order->star_id = $input['star'][$i];
+                    $order->type = $input['type'][$i];
+                    $order->ships = $input['ships'][$i];
+                    $order->save();
+                }
+            } else {
+                if ($input['ships'][$i] == 0) {
+                    $order->type = "C";
+                } else {
+                    $order->type = $input['type'][$i];
+                }
+                $order->ships = $input['ships'][$i];
+                $order->update();
+            }
+        }
+        return view('welcome');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ database/migrations/2017_01_19_190839_create_orderBuys_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrderBuysTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orderBuys_table', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('turn_id');
+            $table->integer('commandant_id');
+            $table->integer('men');
+            $table->string('lasers');
+            $table->integer('ships');
+            $table->integer('robots');
+            $table->integer('missiles');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orderBuys_table');
+    }
+}
